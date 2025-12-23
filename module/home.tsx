6844ca07c5e218b680d92067ed06125388a01486
Arrofii Faiz <122667813+Arropi@users.noreply.github.com>
2025-12-24 03:46:33 +0700

    Fix: tanggal home modules

--- module/home.tsx
+++ module/home.tsx
@@ -2,17 +2,24 @@ import Button from "@/components/button";
 import Link from "next/link";
 
 export default function Home() {
-    const now = new Date().setHours(0,0,0,0);
-    const theDay = new Date(2025,11, 24).setHours(0,0,0,0);
+    const todayWIB = new Date().toLocaleDateString("en-CA", {
+  timeZone: "Asia/Jakarta",
+});
 
-    let TITLE = "Belum sampai hari ulang tahun mu ya puput?";
-    let UCAPAN = ""
-    if (now === theDay) {
-        TITLE = "DEAR PEMBALAP F1 GODEAN."
-        UCAPAN  = "Hari ini salah seorang superstar, penggiat ilmu, pembalap, dan apapun sosok imut itu lahir. Maaf kalau pemberianku tidak se istimewa itu, tidak semeriah itu, dan mungkin apa yang kupersiapkan ini kayak anak SMA baru puber kali ya. Tapi gapapa kalo begitu aku bakalan puber terus dah hehe. Oiya aku ada sedikit ucapan mungkin aku ga pintar ngerangkai katanya tapi semoga maksud dan doa ku tersampaikan."
-    } else if (now > theDay) {
-        TITLE = "Hallo puput f1, hari ulang tahun mu sudah lewat ya? Semoga harimu tetap menyenangkan!"
-    }
+const birthday = "2025-12-24";
+
+let TITLE = "Belum sampai hari ulang tahun mu ya puput?";
+let UCAPAN = "";
+
+if (todayWIB === birthday) {
+  TITLE = "DEAR PEMBALAP F1 GODEAN.";
+  UCAPAN =
+    "Hari ini salah seorang superstar, penggiat ilmu, pembalap, dan apapun sosok imut itu lahir...";
+} else if (todayWIB > birthday) {
+  TITLE =
+    "Hallo puput f1, hari ulang tahun mu sudah lewat ya? Semoga harimu tetap menyenangkan!";
+}
+
   return (
     <div className=" absolute inset-0 flex items-center justify-center flex-col">
         <h1 className="text-4xl font-bold text-white text-shadow-sm text-center">{TITLE}</h1>
